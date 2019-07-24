75e0a56938dac648b2c4a4b3ad7d32224b736c10
DestinyJun <429771890@qq.com>
2019-07-24 20:34:17 +0800

    日志： （1）第二天拖完了

--- 20190721.sql
+++ 20190721.sql
@@ -48,4 +48,49 @@ alter table test_modify modify f3 int unique auto_increment;
 desc test_modify;
 # 添加主键
 alter table test_modify add primary key (f1);
+# 删除列属性
+alter table test_modify modify f1 int;
+alter table test_modify modify f2 char(10);
+alter table test_modify modify f3 int unique;
 desc test_modify;
+# 删除主键 --如果auto_increment与primary 可以连用，那么想删除primary key 先要删除auto_increment
+alter table test_modify modify f1 int unsigned;
+alter table test_modify drop primary key;
+
+-- 创建新表 --
+create table test_unique(
+  f1 char(10) unique
+)charset utf8;
+show create table test_unique;
+# 删除unique
+alter table test_unique drop key f1;
+desc test_unique;
+# 复制表结构
+desc test_modify;
+create table test_modify_bak like test_modify;
+desc test_modify_bak;
+# 备份sql执行结果
+select * from ip_address;
+drop table if exists ip_address_bak;
+create table ip_address_bak select * from ip_address where math = 0;
+select * from ip_address_bak;
+#限制更新
+update ip_address set math = math + 10 where math < 10; -- 全部更新
+update ip_address set math = math + 10 where math > 0 limit 1;
+#限制删除
+delete from ip_address where math <= 10;
+delete from ip_address where math > 10 limit 1;
+# 批量插入
+insert into ip_address_bak  select * from ip_address;
+select * from ip_address_bak;
+# 蠕虫复制
+insert into ip_address_bak (ip,math,sex,aihao) select ip,math,sex,aihao from ip_address;
+select * from ip_address_bak;
+# 主键冲突之有就更新，没有就插入
+insert into ip_address values('195.154.154.144',20,'男','打牌') on duplicate key update math = math + 50;
+insert into ip_address values('195.154.154.144',20,'男','打牌') on duplicate key update math = math + 50;
+# 主键冲突之冲突替换
+replace into ip_address values('195.154.154.144',20,'男','打牌');
+# 清空表
+delete from ip_address_bak; -- 只会删除数据，不会重建索引
+truncate ip_address_bak; -- 不只会删除数据，还会重建索引
