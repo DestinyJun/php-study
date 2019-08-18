2e9d0e0673ca8bf254b0d215f1b1b0b98036b820
DestinyJun <429771890@qq.com>
2019-08-18 14:14:13 +0800

    日志： （1）gb2画图

---
+++ 08gd/01-createimg.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 基本绘制操作
+ */
+$w = 400;
+$h = 200;
+$img = imagecreatetruecolor($w,$h);
+// 分配颜色，只是创建了颜色，但是没有使用
+$bg = imagecolorallocate($img,128,255,187);
+// 为画布填充颜色
+imagefill($img,0,0,$bg);
+// 显示画布
+header('content-type:image/jpeg'); // 告诉浏览器以图片格式来显示
+imagejpeg($img);
+
