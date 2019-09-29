fa0d199a25212365410b8194af0a6a8103f52ab6
DestinyJun <429771890@qq.com>
2019-09-29 20:18:41 +0800

    日志： （1）MVC博客系统开始1

--- 17blog/Home/Conf/Config.php
+++ 17blog/Home/Conf/Config.php
@@ -3,5 +3,16 @@
  * 前台配置文件
  */
 return array(
-
+  // 数据库配置
+  'db_type'=>'mysql',// 数据库剋新
+  'db_host'=>'localhost',// 主机名
+  'db_port'=>'3306',// 端口
+  'db_name'=>'blog',//数据库名
+  'db_user'=>'root',//用户名
+  'db_pass'=>'root',//密码
+  'db_charset'=>'utf8',//字符集
+  // 前台应用默认路由参数
+  'default_platform'=>'Home', // 默认应用
+  'default_controller'=>'Index',// 默认控制器
+  'default_action'=>'action',// 默认行为
 );
