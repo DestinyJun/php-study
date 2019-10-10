22786d8a875a037ae38057e31c10d46cc2cc24eb
DestinyJun <429771890@qq.com>
2019-10-10 22:42:30 +0800

    日志： （1）分离博客系统

--- 17blog/Admin/Model/LineModel.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Admin\Model{
-  use Frame\Libs\BaseModel;
-  final class LineModel extends BaseModel
-  {
-    protected $table = 'links';
-  }
-}
-
--- 17blog/Admin/Model/UserModel.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Admin\Model{
-  use Frame\Libs\BaseModel;
-  final class UserModel extends BaseModel
-  {
-    protected $table = 'user';
-  }
-}
